e493f103fc871ccb1e750d66ac0d4d8ef46824cf
Rafal <rafek131@wp.pl>
2023-12-06 20:02:00 +0100

    Added function sorting posts by date; created first version of main page

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,4 +1,14 @@
-export const slugify = (text: string | number) => {
+type slugifyType = (text: string | number) => string;
+type formatDateType = (date: Date) => string;
+type optionsType = {
+	filterOutDrafts?: boolean;
+	filterOutFuturePosts?: boolean;
+	sortByDate?: boolean;
+	limit?: number | undefined;
+};
+type formatBlogPosts = (posts: any[], {}?: optionsType) => object[];
+
+export const slugify: slugifyType = text => {
 	return text
 		.toString()
 		.toLowerCase()
@@ -9,8 +19,37 @@ export const slugify = (text: string | number) => {
 		.replace(/-+$/, '');
 };
 
-export const formatDate = (date: Date) => {
+export const formatDate: formatDateType = date => {
 	return new Date(date).toLocaleDateString('pl-PL', {
 		timeZone: 'UTC',
 	});
+};
+
+export const formatBlogPosts: formatBlogPosts = (
+	posts,
+	{ filterOutDrafts = true, filterOutFuturePosts = true, sortByDate = true, limit = undefined } = {}
+) => {
+	const filteredPosts = posts.reduce((acc, post) => {
+		const { date, draft } = post.frontmatter;
+		// filterOutDrafts if true
+		if (filterOutDrafts && draft) return acc;
+
+		// filterOutFuturePosts if true
+		if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+		// add post to acc
+		acc.push(post);
+
+		return acc;
+	}, []);
+
+	if (sortByDate) {
+		filteredPosts.sort((a: any, b: any) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+	} else {
+		filteredPosts.sort(() => Math.random() - 0.5);
+	}
+
+	// limit if number is passed
+	if (typeof limit === 'number') filteredPosts.slice(0, limit);
+	return filteredPosts;
 };
